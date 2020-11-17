a2ba045884882cdf0896df7216a0a46c1b9ba246
liuyuhuan <liuyuhuan@babytree-inc.com>
2020-11-17 19:17:34 +0800

    flx

---
+++ my-project/src/pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="index">
+    <div class="head">
+      <h3>必过分期</h3>
+      <p>必过分期，轻松助你逢考必过</p>
+    </div>
+    <div class="list">
+      <div class="listBtn">
+        <div class="left">
+          <h3>我要分期</h3>
+          <p>免息分期，秒速审批，安全可靠的金融助学神器</p>
+        </div>
+        <div class="right"><img src="../../assets/indexicon0.png"/></div>
+      </div>
+
+      <div class="listBtn">
+        <div class="left">
+          <h3>我的订单</h3>
+          <p>可查询申请结果，请按时还款，保持良好信用，逾期未还，拉低信用</p>
+        </div>
+        <div class="right"><img src="../../assets/indexicon0.png"/></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  @import './index.less'
+</style>
